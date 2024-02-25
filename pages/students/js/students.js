f603bfbcf607b517fdc3b286b75bad2ee0b55540
Madimetja94 <fmvundlela@gmail.com>
2024-02-25 16:32:19 +0200

    listing all the details

--- pages/students/js/students.js
+++ pages/students/js/students.js
@@ -14,9 +14,28 @@ document.addEventListener("DOMContentLoaded", async () => {
       const studentApplications = await response.json();
        studentApplications.forEach(application => {
             const tr = document.createElement("tr");
-            const td = document.createElement("td");
-            td.textContent = application.firstName +" "+ application.lastName;
-            tr.appendChild(td);
+            const firstName= document.createElement("td");
+            const lastName =document.createElement("td");
+            const university = document.createElement("td");
+            const amount = document.createElement("td");
+            const status = document.createElement("td");
+            const action = document.createElement("td");
+            const viewButton = document.createElement("button");
+
+            firstName.textContent = application.firstName;
+            lastName.textContent = application.lastName;
+            university.textContent = application.universityName;
+            amount.textContent = `R${application.amount}`;
+            status.textContent = application.fundRequestStatus;
+            viewButton.textContent = "View Application";
+            action.appendChild(viewButton);
+
+            tr.appendChild(firstName);
+            tr.appendChild(lastName);
+            tr.appendChild(university);
+            tr.appendChild(amount);
+            tr.appendChild(status);
+            tr.appendChild(action);
             tbody.appendChild(tr);
        });
     });
